b4f4255cd9f277a5a88a32408025f1b1ce51b41f
James Mak-Tubbs <james.m.tubbs@gmail.com>
2013-05-04 21:16:29 -0400

    Rob ford movement

--- javascript/enemy.js
+++ javascript/enemy.js
@@ -26,6 +26,12 @@ var robFord = {
         config.ford_img, {width:48, height:48}
     ],
     animations: {'static':[0, 1]},
+    controlMapping: {
+        down: null,
+        left: null,
+        right: null,
+        up: null
+    },
     // Have a default start point if the map provides nothing.
     start: {
         x: 500,
@@ -33,6 +39,41 @@ var robFord = {
         map: 0
     }
 };
+
+Enemy.prototype.doMove = function(targetX, targetY) {
+    var xDelta;
+    var yDelta;
+
+    if ((Math.abs(this.rect.left - targetX) > 1) && (Math.abs(this.rect.top - targetX) > 1)) {
+
+        xDelta = targetX - this.rect.left;
+        yDelta = targetY - this.rect.top;
+
+        var vectorLength = Math.sqrt(Math.abs((yDelta*yDelta)+(xDelta*xDelta)));
+
+        var unitX = xDelta / vectorLength;
+        var unitY = yDelta / vectorLength;
+
+        this.rect.left = this.rect.left + unitX; //times speed?
+        this.rect.top = this.rect.top + unitY; //times speed?
+        console.log('Unit Vectors: '+unitX+' '+unitY);
+        console.log('Vector Delta' +xDelta+' '+yDelta);
+        console.log('Vector Length' +vectorLength);
+        console.log('Rectangle Location '+this.rect.left+' '+this.rect.top);
+
+    } else {
+        alert("Reached Destination!");
+    }
+}
+
+Enemy.prototype.update = function(msDuration) {
+    //FourDirection.prototype.update.apply(this, arguments);
+    window.setTimeout(this.doMove(24,24),1000);
+}
+
+// Enemy.protoype.handleEvent = function() {
+//     //to go towards a player
+// }
 
 var initialize = function() {
     return new Enemy(robFord);
